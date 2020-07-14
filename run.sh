12b360afe74115ef0ae1c133b053a1453d9b01a5
rubenchu <gyb0418@gmail.com>
2020-07-14 22:28:51 +0900

    run.sh add cd cmd

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ WINDOW=$2
 CNT=$3
 
 export PYTHONPATH="/workspace/jazzstock_bot:$PYTHONPATH"
+cd /workspace/jazzstock_bot
 git pull origin master
 python3 -u /workspace/jazzstock_bot/main/main_crawlnaver_run.py $INSTANCE_ID $WINDOW $CNT >> debug.log &
