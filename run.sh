76a25a24838108806246546f2cfb46ce044f13f6
rubenchu <gyb0418@gmail.com>
2020-07-17 08:06:10 +0900

    run.sh 에서 환경변수 handling 하도록 변경

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
-INSTANCE_ID=$1
-WINDOW=$2
-CNT=$3
-
+export INSTANCE_ID=$1
+export WINDOW=$2
+export CNT=$3
 export PYTHONPATH="/workspace/jazzstock_bot:$PYTHONPATH"
 cd /workspace/jazzstock_bot
+git checkout -- .
+
+
 git pull origin master
 python3 -u /workspace/jazzstock_bot/main/main_crawlnaver_run.py $INSTANCE_ID $WINDOW $CNT >> debug.log &
