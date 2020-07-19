f994b2f8aca43c8e5b217f2cd05b0f2282ea1c40
rubenchu <jazztronomers@gmail.com>
2020-07-20 07:21:42 +0900

    TELEGRAM BOT 운영용 설정값 수정

--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 export INSTANCE_ID=$1
-<<<<<<< HEAD
-=======
-export CNT=$2
->>>>>>> 3d697077266c1beaf6a54438834dc3937f3b0cef
 export PYTHONPATH="/workspace/jazzstock_bot:$PYTHONPATH"
+DATE=$(date '+%Y%m%d')
 cd /workspace/jazzstock_bot
 git checkout -- .
 
 
 git pull origin master
-<<<<<<< HEAD
-python3 -u /workspace/jazzstock_bot/main/main_crawlnaver_run.py $INSTANCE_ID >> debug_$INSTANCE_ID_.log &
-=======
-python3 -u /workspace/jazzstock_bot/main/main_crawlnaver_run.py $INSTANCE_ID >> debug.log &
->>>>>>> 3d697077266c1beaf6a54438834dc3937f3b0cef
+python3 -u /workspace/jazzstock_bot/main/main_crawlnaver_run.py $INSTANCE_ID >> debug_$INSTANCE_ID_$DATE.log &
+
