86dfb1bd2d44672e6463618328a9c7f2e828eae0
rubenchu <jazztronomers@gmail.com>
2020-07-25 11:34:54 +0900

    시뮬레이션 코드 2차구현

--- crawl/jazzstock_core_simulation.py
+++ crawl/jazzstock_core_simulation.py
@@ -2,7 +2,9 @@ import common.connector_db as db
 import util.index_calculator as ic
 import pandas as pd
 import config.condition as cf
-from crawl.jazzstock_object import JazzstockObject
+from datetime import datetime
+from crawl.jazzstock_object_account import JazzstockObject_Account
+
 
 
 pd.options.display.max_rows = 1000
@@ -24,7 +26,7 @@ condition_dict = cf.TESTCOND
 
 
 class JazzstockCoreSimulation:
-    def __init__(self, stockcode, the_date, the_date_index):
+    def __init__(self, stockcode, the_date, the_date_index, purchased=0, amount=0):
         '''
 
         :param stockcode:
@@ -32,16 +34,20 @@ class JazzstockCoreSimulation:
         :param to:
         :param condition:
         '''
-        self.obj = JazzstockObject(stockcode, the_date, the_date_index)
-
-
+        self.obj = JazzstockObject_Account(stockcode, the_date, the_date_index, purchased, amount)
         self.obj.set_ohlc_day_from_db_include_index(cntto=the_date_index + 1, window=60)       # to + 1, 즉 시뮬레이션 전 거래일 까지의 일봉정보를 가져옴
-        self.obj.set_ohlc_min_from_db(cntto=the_date_index, window=1)             # to 거래일 까지의 5분봉을 가져옴
+        self.obj.set_ohlc_min_from_db(cntto=the_date_index, window=1)                          # to 거래일 까지의 5분봉을 가져옴
         self.obj.set_prev_day_index()
 
         self.obj.df_ohlc_realtime_filled = ic.fillindex(self.obj.df_ohlc_min)     # 5분봉에 지표들을 붙여줌
+        self.obj.df_ohlc_realtime_filled = self.obj.df_ohlc_realtime_filled[self.obj.df_ohlc_realtime_filled.DATE==the_date]
         self.COLUMNS_DAY=['DATE', 'TIME', 'CLOSE', 'VSMAR20', 'BBP', 'BBW', 'K', 'D', 'J']
         self.COLUMNS_MIN = ['DATE', 'TIME', 'CLOSE', 'VSMAR20', 'BBP', 'BBW', 'K', 'D', 'J']
+
+        # DEBUGGING ================================================
+        # print(self.obj.df_ohlc_day.tail(2))
+        # print(self.obj.df_ohlc_realtime_filled.tail(10))
+        # DEBUGGING ================================================
 
 
     def run(self):
@@ -59,34 +65,9 @@ class JazzstockCoreSimulation:
         '''
 
 class JazzstockCoreSimulationCustom(JazzstockCoreSimulation):
-    def __init__(self, stockcode, the_date, the_date_index):
-        super().__init__(stockcode, the_date, the_date_index)
-
-
-
-        # ===================================================================
-        # Condition 만 구현해서 타 객체에서 불러오도록 수정.
-        # 밑에는 일단 간단한 condition
-
-        # SIMPLE & 모두 AND 조건, 위에서 부터 연산을 진행하고, 위에서 False 가 반환되면 밑은 계산하지 않음.
-
-
-
-
-        # 일봉기준으로 전일 저가보다 낮으면서, 5분봉기준 STOCHASTIC D 가 0.2 이하인 종목을 추출하는 조건
-#         condition_simple = {
-#             'CLOSE': ('SMALLER_P', self.obj.PREV_1_LOW, 0.01),
-#             'CLOSE': ('SMALLER_MINMAX_P', self.obj.PREV_1_BBU, self.obj.PREV_1_BBL, 0),
-#             'D': ('SMALLER',0.5)
-#         }
-        
-        
-        condition_dict = cf.TESTCOND 
-        # ===================================================================
-
-        self.condition_list = condition_dict # 순서중요, 순서대로 iteration 돌거
-
-
+    def __init__(self, stockcode, the_date, the_date_index, purchased, amount):
+        super().__init__(stockcode, the_date, the_date_index, purchased, amount)
+        self.condition_list = cf.TESTCOND
 
 
     def simulate(self):
@@ -96,42 +77,100 @@ class JazzstockCoreSimulationCustom(JazzstockCoreSimulation):
         :return:
         '''
 
-        ret = self.obj.simul_all_condition(self.condition_list)
+        purchased, selled = 0, 0
+        st = datetime.now()
+        for row in self.obj.df_ohlc_realtime_filled.values:
+            tempdf = pd.DataFrame(data=[row], columns=self.obj.df_ohlc_realtime_filled.columns)
 
-        if(ret):
-            print('*', self.obj.the_date, ret[0].CLOSE.mean(), ret[0].CLOSE.count(), len(ret))
-            return ret[0].CLOSE.mean(), int(ret[0].CLOSE.count())
-        else:
-            return False
+            print(tempdf)
+
+            ret = self.obj.simul_all_condition_iteration(self.condition_list, tempdf)['result']
+            if ret:
+                res = self.obj.check_status(ret[0])
+
+                if 'purchased' in res.keys():
+                    purchased += res['purchased']
+                elif 'selled' in res.keys():
+                    selled += res['selled']
+
+        return self.obj.purchased, self.obj.amount, self.obj.profit, purchased, selled
+        # ret = self.obj.simul_all_condition(self.condition_list)['result']
+
+
+
+
+
+
+def summary(tpl):
+
+    if tpl[1]>0:
+        avg =tpl[0]/ tpl[1]
+    else:
+        avg = 0
+
+    hist_purchased = tpl[3]- tpl[0]
+    hist_selled = tpl[-1]
+
+    return '평단 %.1f, 누적매수 %.1f, 누적매도 %.1f, 누적수익 %.1f' %(avg, hist_purchased, hist_selled, tpl[2])
+
 
 
 def date_to_index(date):
-
     cnt = db.selectSingleValue("SELECT CNT FROM jazzdb.T_DATE_INDEXED WHERE 1=1 AND DATE = '%s'"%(date))
     return cnt
 
 def index_to_date(idx):
-
-
     thedate = db.selectSingleValue("SELECT DATE FROM jazzdb.T_DATE_INDEXED WHERE 1=1 AND CNT = '%s'"%(idx))
     return thedate
 
+
+
 if __name__=='__main__':
 
+    sl = []
+    stock_dic = {}
 
-    # the_date = '2020-06-26'
-    # the_date_index = date_to_index(the_date)
-    # t = JazzstockCoreSimulationCustom('131370', the_date=the_date, the_date_index=the_date_index)
-    # t.simulate()
-
-    s, c = 0, 0
     
-    for each_idx in range (100,0,-1):
+    for each_idx in range (20, -1 ,-1):
         the_date = index_to_date(each_idx)
-        t = JazzstockCoreSimulationCustom('036800', the_date=the_date, the_date_index=each_idx)
-        rt = t.simulate()
 
-        if rt and rt[1] > 0:
-            s = s+(rt[0]*rt[1])
-            c = c+rt[1]
-            print(s/c)
+        # Q = '''
+        #
+        # SELECT STOCKCODE
+        # FROM jazzdb.T_STOCK_MC
+        # WHERE 1=1
+        # AND DATE = '%s'
+        # AND MC BETWEEN 1.5 AND 1.7;
+        #
+        # '''%(the_date)
+        #
+        # ap = db.selectSingleColumn(Q)
+        # for each in ap:
+        #     if each not in sl:
+        #         sl.append(each)
+        #
+        # print(len(sl))
+
+        sl = ['060590']
+
+        for stockcode in sl:
+
+            if stockcode not in stock_dic.keys():
+                stock_dic[stockcode]= (0, 0, 0, 0 ,0)
+                print(' * %s INIT'%(stockcode))
+            t = JazzstockCoreSimulationCustom(stockcode, the_date=the_date, the_date_index=each_idx, purchased=stock_dic[stockcode][0], amount=stock_dic[stockcode][1])
+            try:
+
+                print(' * START %s / %s ' %(stockcode, the_date))
+                hold_purchased, amount, profit, purchased, selled = t.simulate()
+                temp = list(stock_dic[stockcode])
+                temp[0] = int(hold_purchased)
+                temp[1] = int(amount)
+                temp[2] = temp[2] + int(profit)
+                temp[3] = temp[3] + purchased
+                temp[4] = temp[4] + selled
+                stock_dic[stockcode] = tuple(temp)
+                print(' * END %s / %s' %(stockcode,the_date), stockcode, stock_dic[stockcode], summary(stock_dic[stockcode]))
+            except Exception as e:
+
+                print(' * ERROR %s, %s '%(e, stockcode))
