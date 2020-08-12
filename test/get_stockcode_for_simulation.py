4507ccb89ca376ee6b474dc6fa5f777ff243bbe0
rubenchu <jazztronomers@gmail.com>
2020-08-12 23:03:41 +0900

    conditinally get stockcode list (argparse)

--- test/get_stockcode_for_simulation.py
+++ test/get_stockcode_for_simulation.py
@@ -1,49 +1,78 @@
 import common.connector_db as db
-import sys
+import argparse
 
 '''
 COMMAND LINE에서 ARGV를 받아서 쿼리를 실행하여 반환 받은 결과를
 BASH SCRIPT 로 SPACE SPLITED STRING (LIST) ARGV 넘겨야 할때 사용함
 '''
 
-COUNT = sys.argv[1]
-GROUP = sys.argv[2]
+parser = argparse.ArgumentParser(description='Conditionally get Stockcode List')
+parser.add_argument('--whom', type=str, default='ins', metavar='w',
+                    help='window, option:ins, for, yg, samo')
+parser.add_argument('--window', type=int, default=5, metavar='w',
+                    help='window, option:ins, for, yg, samo')
+parser.add_argument('--seperator', type=str, default='s', metavar='f',
+                     help='s: space separated list, c: comma separated list,')
+parser.add_argument('--row_num_from', type=int, default=0, metavar='s',
+                     help='row_num_from')
+parser.add_argument('--row_num_to', type=int, default=20, metavar='e',
+                     help='row_num_to')
+parser.add_argument('--date_idx', type=int, default=0, metavar='d',
+                     help='date_idx')
+parser.add_argument('--min_market_cap', type=int, default=1, metavar='m',
+                     help='min_market_cap')
+parser.add_argument('--descending', type=int, default=True, metavar='m',
+                     help='descending')
+parser.add_argument('--verbose', type=bool, default=False, metavar='v',
+                     help="print query or not")
 
-query = '''
+args = parser.parse_args()
+
+
+
+WHOM = args.whom
+WINDOW = args.window
+SEPERATOR = ',' if args.seperator=='c' else ' '
+ROW_NUM_FROM= args.row_num_from
+ROW_NUM_TO= args.row_num_to
+DATE_IDX = args.date_idx
+MIN_MARKET_CAP = args.min_market_cap
+DESCENDING = 'DESC' if args.descending else 'ASC'
+
+
+WINDOW_DICT = {
+
+    'ins':'I',
+    'for':'F',
+    'yg':'YG',
+    'per':'PS',
+    'samo':'S'
+
+}
+query = f'''
+
 SELECT STOCKCODE
 FROM
 (
-    SELECT STOCKCODE, 
-            CASE WHEN RN <= 120 THEN "A"
-                 WHEN RN <= 240 THEN "B"
-                 WHEN RN <= 360 THEN "C"
-                 WHEN RN <= 480 THEN "D"
-                 WHEN RN <= 600 THEN "E"
-                 WHEN RN <= 720 THEN "F"
-                 WHEN RN <= 840 THEN "G"
-                 WHEN RN <= 960 THEN "H"
-                 WHEN RN <= 1080 THEN "I"
-                 WHEN RN <= 1200 THEN "J"
-                 ELSE "K" END AS GRP
-    FROM
-    (
-        SELECT STOCKCODE, 
-            ROW_NUMBER() OVER (PARTITION BY DATE ORDER BY I5+F5 DESC) AS RN
-        FROM jazzdb.T_STOCK_SND_ANALYSIS_RESULT_TEMP
-        JOIN jazzdb.T_DATE_INDEXED USING (DATE)
-        JOIN jazzdb.T_STOCK_MC USING (STOCKCODE, DATE)
-        WHERE 1=1
-        AND CNT = 0
-        AND MC > 1          # 1300종목
-    ) A
-) B        
-WHERE 1=1
-AND GRP IN ('%s')
-LIMIT %s
-'''%(GROUP, COUNT)
+    SELECT STOCKCODE,
+        ROW_NUMBER() OVER (PARTITION BY DATE ORDER BY {WINDOW_DICT[WHOM]}{WINDOW} {DESCENDING}) AS RN
+    FROM jazzdb.T_STOCK_SND_ANALYSIS_RESULT_TEMP
+    JOIN jazzdb.T_DATE_INDEXED USING (DATE)
+    JOIN jazzdb.T_STOCK_MC USING (STOCKCODE, DATE)
+    WHERE 1=1
+    AND CNT = {DATE_IDX}
+    AND MC > {MIN_MARKET_CAP}          # 1300종목
+) A
+WHERE 1=1 
+AND RN BETWEEN {ROW_NUM_FROM} AND {ROW_NUM_TO}
+
+'''
+if args.verbose:
+
+    print(f'QUERY:\n{query}\n===========================================')
 
 sl = db.selectSingleColumn(query)         # SELECT 결과를 리스트로 받음
-rt = ' '.join(sl)
+rt = SEPERATOR.join(sl)
 print(rt)
 
 
