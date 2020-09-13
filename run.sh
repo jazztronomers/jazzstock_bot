e7ff8e8993546f4825aeb9c2ac521d5b5c8a3a59
rubenchu <gyb0418@gmail.com>
2020-09-14 00:49:35 +0900

    simulation, daily rebalencing

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export INSTANCE_ID=$1
 export PYTHONPATH="/workspace/jazzstock_bot:/workspace"
 DATE=$(date '+%Y%m%d')
-python3 -u /workspace/jazzstock_bot/main/main_crawlnaver_run.py $INSTANCE_ID >> /workspace/jazzstock_bot/log/debug_$INSTANCE_ID_$DATE.log &
+python3 -u /workspace/jazzstock_bot/main/main_crawl_naver_run.py $INSTANCE_ID >> /workspace/jazzstock_bot/log/debug_$INSTANCE_ID_$DATE.log 2>&1 &
 
