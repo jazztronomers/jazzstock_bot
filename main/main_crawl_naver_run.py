85ddf009de3dc88693d0795eb0fa3061d565ad98
rubenchu <jazztronomers@gmail.com>
2021-01-19 10:20:20 +0900

    instance당 종목수 추가

--- main/main_crawl_naver_run.py
+++ main/main_crawl_naver_run.py
@@ -12,13 +12,13 @@ if __name__=='__main__':
         FROM
         (
             SELECT STOCKCODE, 
-                    CASE WHEN RN <= 120 THEN "A"
-                         WHEN RN <= 240 THEN "B"
-                         WHEN RN <= 360 THEN "C"
-                         WHEN RN <= 480 THEN "D"
-                         WHEN RN <= 600 THEN "E"
-                         WHEN RN <= 720 THEN "F"
-                         WHEN RN <= 840 THEN "G"
+                    CASE WHEN RN <= 200 THEN "A"
+                         WHEN RN <= 400 THEN "B"
+                         WHEN RN <= 600 THEN "C"
+                         WHEN RN <= 800 THEN "D"
+                         WHEN RN <= 1000 THEN "E"
+                         WHEN RN <= 1200 THEN "F"
+                         WHEN RN <= 1400 THEN "G"
                          ELSE "H" END AS GRP
             FROM
             (
@@ -41,7 +41,7 @@ if __name__=='__main__':
     elif len(sys.argv) > 1:
         stockcode_list = sys.argv[1:]
     else:
-        stockcode_list = ['079940', '093320']
+        stockcode_list = ['058470', '115180']
 
     print(' * LEN: %s'%(len(stockcode_list)))
     the_date = datetime.now().date()
